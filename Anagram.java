677aaf6be59a67c56246366f0573e50f918e2680
ido karpel <idokarpel@idos-MacBook-Pro.local>
2025-11-21 16:14:46 +0200

    הודעת קומיט תיאורית

--- Anagram.java
+++ Anagram.java
@@ -28,22 +28,70 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		String pStr1 = preProcess(str1);
+        String pStr2 = preProcess(str2);
+        
+        if (pStr1.length() != pStr2.length()) {
+            return false;
+        }
+        
+        if (pStr1.length() == 0) {
+            return true;
+        }
+        
+        char[] arr1 = pStr1.toCharArray();
+        char[] arr2 = pStr2.toCharArray();
+        
+        java.util.Arrays.sort(arr1);
+        java.util.Arrays.sort(arr2);
+        
+        String sortedStr1 = new String(arr1);
+        String sortedStr2 = new String(arr2);
+        
+        return sortedStr1.equals(sortedStr2);
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		StringBuilder sb = new StringBuilder();
+        
+        
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            
+            
+            if (Character.isLetter(c)) {
+                
+                sb.append(Character.toLowerCase(c));
+            }
+           
+        }
+        
+        return sb.toString();
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		java.util.List<Character> characters = new java.util.ArrayList<>();
+        
+        for (char c : str.toCharArray()) {
+            characters.add(c);
+        }
+        
+        java.util.Random random = new java.util.Random();
+        StringBuilder sb = new StringBuilder();
+        
+        while (!characters.isEmpty()) {
+            int randomIndex = random.nextInt(characters.size());
+            
+            sb.append(characters.get(randomIndex));
+            
+            characters.remove(randomIndex);
+        }
+        
+        return sb.toString();
 	}
 }
